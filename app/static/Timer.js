a706836d9e8a7d13ca2889a9a17204003c5594e5
Your Name <you@example.com>
2022-04-14 22:36:28 -0700

    4/14 timer function down

--- app/static/Timer.js
+++ app/static/Timer.js
@@ -4,26 +4,39 @@ export default class Timer {
      root.innerHTML = Timer.getHTML();
 
      this.el = {
-         minutes: root.querySelector("timer-part-minute"),
-         seconds: root.querySelector("timer-part-seconds"),
-         control: root.querySelector("timer-button-control"),
-         reset: root.querySelector("timer-button-reset")
+         minutes: root.querySelector(".timer-part-minute"),
+         seconds: root.querySelector(".timer-part-seconds"),
+         control: root.querySelector(".timer-button-control"),
+         reset: root.querySelector(".timer-button-reset")
      };
 
      console.log(this.el);
 
      this.interval = null;
-     this.remainingSeconds = 90;
+     this.remainingSeconds = 0;
 
-     this.updateInterfaceTime();
+     this.start();
+     
 
      this.el.control.addEventListener("click", () => {
+        if(this.interval === null){
+            this.start();
+        }else {
+            this.stop();
+        }
 
-
-        });
+    });
 
      this.el.reset.addEventListener("click", ()=> {
+            const inputMin = prompt("Enter # of mins: ")
 
+            if(inputMin <= 60) {
+                this.stop();
+                this.remainingSeconds = inputMin * 60;
+                this.updateInterfaceTime();
+            }else{
+                alert("Cannot enter time more than 60 min");
+            }
 
         });
     }    
@@ -31,9 +44,49 @@ export default class Timer {
     updateInterfaceTime(){
     const minutes = Math.floor(this.remainingSeconds / 60);
     const seconds = this.remainingSeconds % 60;
-    console.log(minutes, seconds);
+    
+    this.el.minutes.textContent = minutes.toString().padStart(2,"0")
+    this.el.seconds.textContent = seconds.toString().padStart(2,"0")
+    }
+
+    updateInterfaceControl(){
+        if (this.interval === null){
+            document.querySelector("#controlButton").textContent = "Start";
+            this.el.control.classList.add(".timer-button-start");
+            this.el.control.classList.remove(".timer-button-stop");
+        }else{
+            document.querySelector("#controlButton").textContent = "Pause";
+            this.el.control.classList.add(".timer-button-stop");
+            this.el.control.classList.remove(".timer-button-start");
+            
+        }
 
     }
+
+    start() {
+        if(this.remainingSeconds === 0 )
+            return;
+
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateInterfaceTime();
+
+        if(this.remainingSeconds === 0) {
+            this.stop();
+        }
+        }, 1000);
+
+        this.updateInterfaceControl();
+    }
+
+    stop() {
+       clearInterval(this.interval);
+
+       this.interval = null;
+
+       this.updateInterfaceControl();
+    }
+
 
     static getHTML() {
         return `
@@ -41,7 +94,7 @@ export default class Timer {
          <span class="timer-part timer-part-minute">00</span>
          <span class="timer-part">:</span>
          <span class="timer-part timer-part-seconds">00</span>
-         <button type="button" class="timer-button timer-button-control timer-button-start" >Start</button>
+         <button type="button" class="timer-button timer-button-control timer-button-start" id="controlButton">Start</button>
          <button type="button" class="timer-button timer-button-reset" >Reset</button>
 
 
